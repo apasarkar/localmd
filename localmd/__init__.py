8adeca020087c5e3fd863e4e03924d365461f57c
apasarkar <amolpasarkar@gmail.com>
2024-10-17 02:49:05 +0800

    Adds low rank svd computation to the high level api

--- localmd/__init__.py
+++ localmd/__init__.py
@@ -1,3 +1,3 @@
-from .decomposition import localmd_decomposition, svd_new_temporal
+from .decomposition import localmd_decomposition, svd_new_temporal, compute_lowrank_factorized_svd
 from .pmdarray import PMDArray
 from .dataset import TiffArray
